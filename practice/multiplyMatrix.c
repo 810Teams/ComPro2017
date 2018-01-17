e07c26a4422ca131606b5372ae772e19ffb50551
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2018-01-17 16:49:23 +0700

    Code Optimized

--- practice/multiplyMatrix.c
+++ practice/multiplyMatrix.c
@@ -4,8 +4,9 @@ Teerapat Kraisrisirikul
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-float multiply(float *matrix_a, float *matrix_b, int p, int q, int r, int select_i, int select_j);
+float multiply(float **matrix_a, float **matrix_b, int q, int select_i, int select_j);
 
 int main() {
     /* Main function */
@@ -15,7 +16,15 @@ int main() {
     printf("q: "); scanf("%i", &q);
     printf("r: "); scanf("%i", &r);
 
-    float matrix_a[p][q], matrix_b[q][r], matrix_result[p][r];
+    float *matrix_a[p], *matrix_b[q], matrix_result[p][r];
+
+    // Memory Allocation for pointer matrices
+    for (int i = 0; i < p; i++) {
+        matrix_a[i] = (float*)malloc((q+1)*sizeof(float));
+    }
+    for (int i = 0; i < q; i++) {
+        matrix_b[i] = (float*)malloc((r+1)*sizeof(float));
+    }
 
     // Input values for matrix_a
     printf("- Matrice A -\n");
@@ -38,8 +47,8 @@ int main() {
     // Calculate result matrix
     for (int i = 0; i < p; i++) {
         for (int j = 0; j < r; j++) {
-            matrix_result[i][j] = multiply(*matrix_a, *matrix_b, p, q, r, i, j);
-            printf("%.2f ", matrix_result[i][j]);
+            matrix_result[i][j] = multiply(matrix_a, matrix_b, q, i, j);
+            printf("%g\t", matrix_result[i][j]);
         }
         printf("\n");
     }
@@ -47,13 +56,13 @@ int main() {
     return 0;
 }
 
-float multiply(float *matrix_a, float *matrix_b, int p, int q, int r, int select_i, int select_j) {
+float multiply(float **matrix_a, float **matrix_b, int q, int select_i, int select_j) {
     /* Multiply matrice a's row with matrice b's column */
     float result = 0;
 
     // Calculate Result
     for (int i = 0; i < q; i++) {
-        result += matrix_a[i + select_i * q] * matrix_b[i * r + select_j];
+        result += (matrix_a[select_i][i] * matrix_b[i][select_j]);
     }
 
     return result;
--- practice/scaledMatrix.c
+++ practice/scaledMatrix.c
@@ -4,9 +4,10 @@ Teerapat Kraisrisirikul
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-float min(float *matrix, int size_i, int size_j);
-float max(float *matrix, int size_i, int size_j);
+float min(float **matrix, int size_i, int size_j);
+float max(float **matrix, int size_i, int size_j);
 
 int main() {
     /* Main function */
@@ -15,7 +16,10 @@ int main() {
     printf("Column size: "); scanf("%i", &size_i);
     printf("Row size: "); scanf("%i", &size_j);
 
-    float matrix[size_i][size_j];
+    float *matrix[size_i];
+    for (int i = 0; i < size_i; i++) {
+        matrix[i] = (float*)malloc((size_j+1)*sizeof(float));
+    }
 
     // Input values for matrix
     for (int i = 0; i < size_i; i++) {
@@ -26,8 +30,8 @@ int main() {
     }
 
     // Get minimum and maximum values
-    float matrix_min = min(*matrix, size_i, size_j);
-    float matrix_max = max(*matrix, size_i, size_j);
+    float matrix_min = min(matrix, size_i, size_j);
+    float matrix_max = max(matrix, size_i, size_j);
 
     // Scale I: Move minimum to 0
     for (int i = 0; i < size_i; i++) {
@@ -46,7 +50,7 @@ int main() {
     // Show matrix
     for (int i = 0; i < size_i; i++) {
         for (int j = 0; j < size_j; j++) {
-            printf("%.2f ", matrix[i][j]);
+            printf("%g\t", matrix[i][j]);
         }
         printf("\n");
     }
@@ -54,24 +58,28 @@ int main() {
     return 0;
 }
 
-float min(float *matrix, int size_i, int size_j) {
+float min(float **matrix, int size_i, int size_j) {
     /* Find minimum value of the matrix */
-    float value = matrix[0];
-    for (int i = 0; i < size_i*size_j; i++) {
-        if (matrix[i] < value) {
-            value = matrix[i];
+    float value = matrix[0][0];
+    for (int i = 0; i < size_i; i++) {
+        for (int j = 0; j < size_j; j++) {
+            if (matrix[i][j] < value) {
+                value = matrix[i][j];
+            }
         }
     }
 
     return value;
 }
 
-float max(float *matrix, int size_i, int size_j) {
+float max(float **matrix, int size_i, int size_j) {
     /* Find maximum value of the matrix */
-    float value = matrix[0];
-    for (int i = 0; i < size_i*size_j; i++) {
-        if (matrix[i] > value) {
-            value = matrix[i];
+    float value = matrix[0][0];
+    for (int i = 0; i < size_i; i++) {
+        for (int j = 0; j < size_j; j++) {
+            if (matrix[i][j] > value) {
+                value = matrix[i][j];
+            }
         }
     }
 
